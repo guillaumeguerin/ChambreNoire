42a5e5ce934f7bbaee4b7297e923e9b8b9d6ce87
guillaumeguerin <guerin_guillaume@hotmail.fr>
2016-12-21 22:57:36 +0100

    init

---
+++ main.js
@@ -0,0 +1,25 @@
+const {app, BrowserWindow} = require('electron');
+const express = require('express')();
+const http = require('http').Server(express);
+const io = require('socket.io')(http);
+
+let mainWindow;
+
+app.on('ready', () => {
+	mainWindow = new BrowserWindow({
+		height: 600,
+		width: 800
+	});
+
+	mainWindow.loadURL('file://' + __dirname + '/index.html');
+  
+	io.on('connection', function(socket){
+	  console.log('a user connected');
+	  socket.on('disconnect', function(){
+		console.log('user disconnected');
+	  });
+	});
+	http.listen(3000, function(){
+	  console.log('listening on *:3000');
+	});
+});
